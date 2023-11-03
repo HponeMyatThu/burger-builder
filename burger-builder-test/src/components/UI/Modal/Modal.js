c539332f86f29639be9d734f087d53f85d23247b
HponeMyatThu <hponemy8tthu@gmail.com>
2023-11-03 23:44:54 +0630

    [ basic ] burger-builder-react

--- burger-builder-test/src/components/UI/Modal/Modal.js
+++ burger-builder-test/src/components/UI/Modal/Modal.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
 import classes from '../Modal/Modal.module.css';
 import Auxilitary from '../../../hoc/Auxilitary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Model = props => (
-  <Auxilitary>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
+class Model extends Component{
+
+  shouldComponentUpdate(nextProps, prevState){
+    console.log("next",nextProps.show);
+    console.log('props', this.props.show, nextProps.show !== this.props.show)
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate(){
+    console.log('[Model.js] componentWillUpdate');
+  }
+
+  render(){
+    return (
+      <Auxilitary>
+    <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
     <div
       className={classes.Modal}
       style={{
-        transform: props.show ? 'translateY(0%)' : 'translateY(-100%)',
-        opacity: props.show ? '1' : '0',
+        transform: this.props.show ? 'translateY(0%)' : 'translateY(-100%)',
+        opacity: this.props.show ? '1' : '0',
       }}
     >
-      {props.children}
+      {this.props.children}
     </div>
   </Auxilitary>
-);
+    )
+  }
+}
 
 export default Model;
